718681f02958ae728ad7984d343fb86c0175712f
Boaz Rosenan <brosenan@gmail.com>
2013-04-01 15:03:48 +0300

    Fibonacci implementation in Cedalion works interpreted in Detalion. Interpreter is still partial and is missing annotation removal, rewrite rules, and most of Cedalion's builtin predicates.

--- detalion/runDetalion.js
+++ detalion/runDetalion.js
@@ -16,9 +16,42 @@ app.on(["detalion", "program"], function() {
 	var det = new Interpreter(program);
 	createBuiltins(det);
 
-	var Y = det.heapAllocate();
-	if(!det.call(['test#fib', 20, Y])) {
+/*	var Y = det.heapAllocate();
+	if(!det.call(['test#fib', 10, Y])) {
 		console.error('fib failed');
 	}
-	console.log('fib emitted ' + JSON.stringify(det.deref(Y)));
+	console.log('fib emitted ' + JSON.stringify(det.deref(Y)));*/
+
+/*	var X = det.heapAllocate();
+	var Y = det.heapAllocate();
+	if(!det.call(['/detalion/cedalion#cedalion', ['/detalion/cedalion#test', Y], Y, ['/bootstrap#number'], X])) {
+		console.error('Cedalion fib failed');
+	}
+	console.log('fib emitted ' + JSON.stringify(det.deepDeref(X)));
+*/
+
+
+/*	var L = det.heapAllocate();
+	var X = det.heapAllocate();
+	if(!det.call(['/detalion/cedalion#map', det.arrayToList([1, 2, 3]), X, ['/bootstrap#number'], L, ['x', X], ['foo']])) {
+		console.error('map failed');
+	}
+	console.log('map emitted ' + JSON.stringify(det.deepDeref(L)));
+*/	
+
+	var X = det.heapAllocate();
+	var Y = det.heapAllocate();
+	if(!det.call(['/detalion/cedalion#cedalion', ['/detalion/cedalion#fib2', 20, Y], Y, ['/bootstrap#number'], X])) {
+		console.error('Cedalion fib failed');
+	}
+	console.log('fib emitted ' + JSON.stringify(det.deepDeref(X)));
+
+/*	var X = det.heapAllocate();
+	var Y = det.heapAllocate();
+	if(!det.call(['/detalion/cedalion#cedalion', ['/detalion/cedalion#toPeano', 2, Y], Y, ['/bootstrap#number'], X])) {
+		console.error('Cedalion fib failed');
+	}
+	console.log('fib emitted ' + JSON.stringify(det.deepDeref(X)));
+*/
+	
 });
